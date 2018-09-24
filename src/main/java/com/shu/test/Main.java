c9558a5d542e38c206d6eb38d2ba164f9cf79f8f
shu616048151 <6160048151@qq.com>
2018-09-24 18:16:42 +0800

    第一提交

---
+++ src/main/java/com/shu/test/Main.java
@@ -0,0 +1,65 @@
+package com.shu.test;
+
+import java.util.UUID;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import com.shu.MyJedisPool;
+
+import redis.clients.jedis.Jedis;
+/**
+ * redis乐观锁的方式实现秒杀
+ * @author shuxibing
+ *
+ */
+public class Main {
+	public static void main(String[] args) {
+		String key="wecash";
+		long startTime=System.currentTimeMillis();
+		//初始化秒杀的数量
+		initNum(key, 200);
+		//当前的秒杀人数,线程池的效率高一些
+		long endTime=startManyThread(key, 1000);
+		System.out.println("总共执行时间："+(endTime-startTime)+"ms");
+	}
+	/**
+	 * 
+	 * @param key 秒杀的商品
+	 * @param num 秒杀的数量
+	 */
+	public static void initNum(String key,int num){
+		Jedis jedis=MyJedisPool.getJedis();
+		jedis.set(key, String.valueOf(num));
+		jedis.close();
+	}
+	/**
+	 * 
+	 * @param key 秒杀的商品
+	 * @param n 秒杀的人数
+	 *
+	 */
+	public static long startManyThread(String key,int n) {
+		ExecutorService executor = Executors.newFixedThreadPool(30);
+		if (n>0) {
+			for (int i = 0; i < n; i++) {
+				String id=UUID.randomUUID().toString().replace("-", "");
+				executor.execute(new com.shu.test.MyThread(i+"用户ID:"+id, key));
+			}
+		}
+		executor.shutdown();
+		//确认线程是否完全结束
+		while(true){
+			if (executor.isTerminated()) {
+				break;
+			}
+			try {
+				Thread.sleep(10);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		return System.currentTimeMillis();
+	}
+	
+}
---
+++ src/main/java/com/shu/test/MyThread.java
@@ -0,0 +1,77 @@
+package com.shu.test;
+
+import org.apache.commons.lang3.StringUtils;
+import org.omg.Messaging.SyncScopeHelper;
+
+import com.shu.MyJedisPool;
+
+import redis.clients.jedis.Jedis;
+/**
+ * 分布式锁
+ * @author Administrator
+ *
+ */
+public class MyThread implements Runnable{
+	final String key="lock";
+	final long timeout=100;
+	Jedis jedis=MyJedisPool.getJedis();
+	private String productKey;
+	private String customerId;
+	
+	public MyThread( String customerId,String productKey) {
+		super();
+		this.productKey = productKey;
+		this.customerId = customerId;
+	}
+	public void run() {
+		long end=System.currentTimeMillis()+timeout;
+		boolean lock=lock(key, String.valueOf(end));
+		int num=Integer.parseInt(jedis.get(productKey));
+		if (lock&&num>0&&num<=200) {
+		
+				jedis.decr(productKey);
+				System.out.println(customerId+"抢购成功,剩余数："+num);
+				unlock(key, String.valueOf(end));
+		}else {
+			if (num<=0) {				
+				System.out.println(customerId+"商品抢购完了");
+			}else {
+				System.out.println(customerId+"抢购失败");
+			}
+		}
+		jedis.close();
+	}
+	public boolean lock(String key,String value){
+	    if(jedis.setnx(key, value)==1){//setNX 返回boolean
+	        return true;
+	    }
+	    //如果锁超时 ***
+	    String currentValue = jedis.get(key);
+	    if(!StringUtils.isEmpty(currentValue) && Long.parseLong(currentValue)<System.currentTimeMillis()){
+	        //获取上一个锁的时间
+	        String oldvalue  = jedis.getSet(key,value);
+	        if(!StringUtils.isEmpty(oldvalue)&&oldvalue.equals(currentValue)){
+	            return true;
+	        }
+	    }
+	    return false;
+	}
+	/***
+	 * 解锁
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public void unlock(String key,String value){
+	    try {
+	        String currentValue = jedis.get(key);
+	        if(!StringUtils.isEmpty(currentValue)&&currentValue.equals(value)){
+	            jedis.del(key);
+	        }
+	    } catch (Exception e) {
+	        System.out.println("解锁异常");
+	    }
+	}
+
+
+}
